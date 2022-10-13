154c90b229a28af2c0551ab94a5e45c31e735a05
Лемов Никита Леонидович <nlemov@phoenixit.ru>
2022-10-13 15:12:14 +0300

    add form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Form = () => {
   return (
     <>
-        
+      <form action="" className="form">
+        <input type="text"/>
+        <input type="text"/>
+        <input type="text"/>
+        <input type="text"/>
+      </form>
     </>
   )
 }
